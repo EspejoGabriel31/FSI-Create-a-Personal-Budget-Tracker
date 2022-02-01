25b3e5595b5b1cb886e88d21cecda1f666ddfa72
unknown <gespejo2144@gmail.com>
2022-01-31 19:04:42 -0800

    did assignment

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+//let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+//let numberAnswer = parseFloat(stringAnswer)
+//console.log(numberAnswer)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    //let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    //console.log(answer)
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[i]))
+    annualExpenses = annualExpenses + answer
+}
